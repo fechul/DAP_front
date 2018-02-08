370aeb2a9270a5f3ca0cd6b3a76e2d2a94884ad6
fechul <nice0612@gmail.com>
2018-02-08 22:41:16 +0900

    add radarChart

--- js/test.js
+++ js/test.js
@@ -498,62 +498,87 @@ var TEST = {
 
 	// 커리어 path 차트 생성
 	makeCareer: function() {
-		var config = {
-			width: 250,
-			height: 250
+		var w = 150,
+			h = 150;
+
+		var colorscale = d3.scale.category10();
+
+		//Legend titles
+		var LegendOptions = ['Ability'];
+
+		//Data
+		var d = [
+				  [
+					{axis:"Java",value:0.59},
+					{axis:"Python",value:0.56},
+					{axis:"C",value:0.42},
+					{axis:"Javascript",value:0.34},
+					{axis:"C++",value:0.48},
+					{axis:"Go",value:0.14}
+				  ]
+				];
+
+		//Options for the Radar chart, other than default
+		var mycfg = {
+		  w: w,
+		  h: h,
+		  maxValue: 1,
+		  levels: 6,
+		  ExtraWidthX: 300
 		}
 
-		var careerData = [
-			{axis: "Axis1", value: 13}, 
-   			{axis: "Axis2", value: 1}, 
-   			{axis: "Axis3", value: 8},  
-   			{axis: "Axis4", value: 4},  
-   			{axis: "Axis5", value: 9},
-			{axis: "Axis6", value: 9}
-		]
+		//Call function to draw the Radar chart
+		//Will expect that data is in %'s
+		RadarChart.draw("#careerChart", d, mycfg);
 
-		var legendOption = ['Career Path'];
-		RadarChart.draw('#careerChart', careerData, config);
-		
+		////////////////////////////////////////////
+		/////////// Initiate legend ////////////////
+		////////////////////////////////////////////
+
 		var svg = d3.select('#careerChart')
-						.append('svg')
-							.attr('width', config.width + 100)
-							.attr('height', config.height)
-		;
-		var text = svg.append('text')
-						.attr('class', 'radarChart_title')
-						.attr('transform', 'translate(90, 0)')
-						.attr('x', w - 70)
-						.attr('y', 10)
-						.attr('font-size', '12px')
-						.attr('fill', '#404040')
-						.text('범례')
-		;
-		var legend = svg.append('g')
-							.attr('class', 'legend')
-							.attr('height', 100)
-							.attr('width', 200)
-							.attr('transform', 'translate(90, 20)')
-		;
-		legend.selectAll('rect')
-				.data(legendOption)
-				.enter()
-				.append('rect')
-					.attr('x', w - 65)
-					.attr('y', function(d, i) { return i * 20 })
-					.attr('width', 10)
-					.attr('height', 10)
-					.style('fill', 'red')
-		;
-		legend.selectAll('text')
-				.data(legendOption)
-				.enter()
-					.append('text')
-					.attr('x', w - 52)
-					.attr('y', function(d, i) { return i * 20 + 9 })
-					.attr('font-size', '11px')
-					.attr('fill', '#737373')
-					.text(function(d) { return d; })
-		;
+			.selectAll('svg')
+			.append('svg')
+			.attr("width", w+300)
+			.attr("height", h)
+
+		//Create the title for the legend
+		// var text = svg.append("text")
+		// 	.attr("class", "title")
+		// 	.attr('transform', 'translate(90,0)') 
+		// 	.attr("x", w - 70)
+		// 	.attr("y", 10)
+		// 	.attr("font-size", "12px")
+		// 	.attr("fill", "#404040")
+		// 	.text("What % of owners use a specific service in a week");
+				
+		//Initiate Legend	
+		var legend = svg.append("g")
+			.attr("class", "legend")
+			.attr("height", 100)
+			.attr("width", 200)
+			.attr('transform', 'translate(90,20)') 
+			;
+			//Create colour squares
+			legend.selectAll('rect')
+			  .data(LegendOptions)
+			  .enter()
+			  .append("rect")
+			  .attr("x", w - 65)
+			  .attr("y", function(d, i){ return i * 20;})
+			  .attr("width", 10)
+			  .attr("height", 10)
+			  .style("fill", function(d, i){ return colorscale(i);})
+			  ;
+			//Create text next to squares
+			legend.selectAll('text')
+			  .data(LegendOptions)
+			  .enter()
+			  .append("text")
+			  .attr("x", w - 52)
+			  .attr("y", function(d, i){ return i * 20 + 9;})
+			  .attr("font-size", "11px")
+			  .attr("fill", "#737373")
+			  .text(function(d) { return d; })
+			  ;	
 	}
 };
